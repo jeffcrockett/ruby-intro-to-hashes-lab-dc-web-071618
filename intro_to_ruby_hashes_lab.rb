5f3ec7f72c48f0a006658e8c53c7c795afdaa6c5
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-01 21:27:40 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,7 +45,7 @@ def monopoly_with_fourth_tier
 	monopoly = monopoly_with_third_tier
 	monopoly[:railroads][:names][:reading_railroad]['mortgage_value'] = '$100'
 	monopoly[:railroads][:names][:pennsylvania_railroad]['mortgage_value'] = '$200'
-	monopoly[:railroads][:names][:b_and_o_railroad]['mortgage_value'] = '$200'
+	monopoly[:railroads][:names][:b_and_o_railroad]['mortgage_value'] = '$400'
 	
 
 end
